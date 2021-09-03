059cc0c95c7060f2aa422c095cdddc1f9fa250ca
Natasha <njohnson533@gmail.com>
2021-09-03 13:13:48 -0400

    Added player factory function
    Player function returns players name and marker

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
 const gameboardContainer = document.querySelector(".card__content");
+const gameboard = (function () {
+	const gameboard = Array(9).fill("");
 
+	const displayBoard = function () {
+		gameboard.forEach(function (gameSpace, index) {
+			gameSpace = document.createElement("article");
+			gameSpace.classList.add("card__item");
+			gameSpace.addEventListener("click", updateBoard);
+			gameSpace.textContent = "";
+			gameSpace.dataset.id = index;
+			gameboardContainer.appendChild(gameSpace);
+			gameboard.push(gameSpace);
+		});
+	};
 
-
-const gameboard = (function() {
-    const gameboard = Array(9).fill("");
-    const displayBoard = function () {
-        console.log(gameboard.length);
-        
-        gameboard.forEach(function (gameSpace, index) {
-            gameSpace = document.createElement("article");
-            gameSpace.classList.add("card__item");
-            gameSpace.addEventListener("click", updateBoard);
-            gameSpace.textContent = "";
-            gameSpace.dataset.id = index;
-            gameboardContainer.appendChild(gameSpace);
-            gameboard.push(gameSpace);
-        });
-        
-    };
-    const updateBoard = function () {
-        
-    };
-    return { displayBoard };
+    const updateBoard = function (selection, symbol) {
+        console.log(selection);
+        console.log(symbol);
+		gameboardContainer.children[selection].textContent = symbol;
+	};
+	return { displayBoard, updateBoard };
 })();
-
-function updateBoard() {
-    gameboardContainer.children[this.dataset.id].textContent = "X";
-}
-
 gameboard.displayBoard();
+
+const Player = function (name, marker) {
+	const getName = function (name) {
+		return name;
+	};
+	const getSymbol = function (marker) {
+		return marker;
+	};
+	
+	return { name, marker };
+};
+const natasha = Player("natasha", "X");
+natasha.getName;
+natasha.getMarker;
+console.log(natasha);
+gameboard.updateBoard(natasha.getName, natasha.getSymbol);
