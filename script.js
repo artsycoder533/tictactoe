ed6d59cb37d9be76ef868740d6e9b6f2ab308a28
Natasha <njohnson533@gmail.com>
2021-09-04 17:35:16 -0400

    feature update
    added event listener to start game button that displays board and adds event listeners to the gamespaces, and starts the game.

--- script.js
+++ script.js
@@ -1,107 +1,114 @@
-const gameboardContainer = document.querySelector(".card__content");
-const startGame = document.querySelector(".card_button");
-
-
 
 const Gameboard = (function () {
+	const gameboardContainer = document.querySelector(".card__content");
     const gameboardArray = Array(9).fill("");
 	const displayBoard = function () {
 		gameboardArray.forEach(function (gameSpace, index) {
 			gameSpace = document.createElement("article");
 			gameSpace.classList.add("card__item");
-			gameSpace.addEventListener("click", updateBoard);
 			gameSpace.textContent = gameboardArray[index];
 			gameSpace.dataset.id = index;
 			gameboardContainer.appendChild(gameSpace);
 		});
 	};
 
-    const updateBoard = function () {
-        //console.log(playerMarker.target);
-        //add check if space is empty
-        
-        if (gameboardArray[this.dataset.id]) {
-            displayMessage("danger");
-            return;
-        }
-        displayMessage("success");
-        gameboardContainer.children[this.dataset.id].textContent = "N";
-        gameboardArray[this.dataset.id] = "N";
-        console.log(gameboardArray);
-    };
-
-    const displayMessage = function (status) {
-        const getMessage = document.querySelector(".card__message");
-        if (status === "danger") {
-            getMessage.classList.add(`card__message--${status}`);
-            getMessage.textContent = "Spot already take, please enter a valid move!";
-            getMessage.classList.add(`card__message--${status}`);
-            setTimeout(function () {
-                getMessage.textContent = "";
-                getMessage.classList.remove(`card__message--${status}`);
-            }, 1500);
-        }
-        else {
-            getMessage.classList.add(`card__message--${status}`);
-            getMessage.textContent = "Valid move!";
-            getMessage.classList.add(`card__message--${status}`);
-            setTimeout(function () {
-                getMessage.textContent = "";
-                getMessage.classList.remove(`card__message--${status}`);
-            }, 1500);
-        }
-    };
-
-    const clearBoard = function () {
-        gameboardArray.forEach(function(spot, index){
-            spot[index] = "";
-            gameboardContainer.children[index].textContent = "";
-        });
-    }
-    
-	return { displayBoard, clearBoard, updateBoard};
-}());
-Gameboard.displayBoard();
-
-const Player = function (name, marker, selection) {
-    //const {displayBoard} = Gameboard(selection)
-	const getName = function () {
-		return name;
+	const updateBoard = function (mark, index) {
+        console.log(mark, index);
+		if (gameboardArray[index]) {
+			displayMessage("danger");
+			return;
+		}
+		displayMessage("success");
+		gameboardContainer.children[index].textContent = mark;
+		gameboardArray[index] = mark;
+		console.log(gameboardArray);
 	};
+
+	const getBoard = function () {
+		return gameboardContainer;
+	};
+
+	const displayMessage = function (status) {
+		const getMessage = document.querySelector(".card__message");
+		if (status === "danger") {
+			getMessage.classList.add(`card__message--${status}`);
+			getMessage.textContent = "Spot already take, please enter a valid move!";
+			getMessage.classList.add(`card__message--${status}`);
+			setTimeout(function () {
+				getMessage.textContent = "";
+				getMessage.classList.remove(`card__message--${status}`);
+			}, 1500);
+		} else {
+			getMessage.classList.add(`card__message--${status}`);
+			getMessage.textContent = "Valid move!";
+			getMessage.classList.add(`card__message--${status}`);
+			setTimeout(function () {
+				getMessage.textContent = "";
+				getMessage.classList.remove(`card__message--${status}`);
+			}, 1500);
+		}
+	};
+
+	const clearBoard = function () {
+		gameboardArray.forEach(function (spot, index) {
+			spot[index] = "";
+			gameboardContainer.children[index].textContent = "";
+		});
+	};
+
+	return { displayBoard, clearBoard, updateBoard, getBoard };
+}());
+
+
+const Player = function (marker) {
+    this.marker = marker;
 	const getMarker = function () {
 		return marker;
     };
-    const getSelection = function () {
-        return selection;
-        //gameboardContainer.children[id].textContent = symbol;
-    };
-    //const { updateBoard } = Gameboard.updateBoard();
-    
-	return { getName, getMarker, getSelection};
+
+	return { getMarker};
 };
-const player1 = Player("natasha", "X");
-const player2 = Player("computer", "O");
 
-//gameboard.updateBoard(player1.getName(), player1.getMarker())
+const Game = (function () {
+    const startGame = document.getElementById("start");
+    startGame.addEventListener("click", function () {
+        Gameboard.displayBoard();
+        console.log("start game");
+        const board = Gameboard.getBoard().children;
+        let spaces = [...board];
+        spaces.forEach(function (space) {
+            space.addEventListener("click", playGame);
+        });
+    });
+	const currentPlayer = "player1";
+	const player1 = Player("X");
+	const player2 = Player("O");
+	const gameOver = false;
+	const winner = "";
+	const getPlayer1 = function () {
+		return player1;
+	};
+	const getPlayer2 = function () {
+		return player2;
+	};
 
-const Game = (function (playerA, playerB) {
-    const gameOver = false;
-    const winner = "";
     const playGame = function () {
-        while (!winner && gameOver) {
-            console.log(playerA.getMarker());
-            playerA.updateBoard();
-        }
-    }
-    const getPlayerTurn = function () {
-        //console.log(Player.getMarker());
-        //Gameboard.updateBoard(Player.getMarker());
-    }
-    return { playGame, getPlayerTurn};
-})();
+        console.log(this);
+        console.log("inside play game");
+        Gameboard.updateBoard(getPlayer1().getMarker(), this.dataset.id)
+        return;
+	};
+	const getPlayerTurn = function (currentPlayer) {
+		if (currentPlayer === player1) {
+			return getPlayer1();
+		}
+		if (currentPlayer === player2) {
+			return getPlayer2();
+		}
+	};
+	//startGame.addEventListener("click", playGame);
+	return { playGame, getPlayerTurn, getPlayer1, getPlayer2, getPlayerTurn };
+}());
 
-// Game.playGame(player1, player2);
 
-const Foo = (function () {
-    
-})();
+
